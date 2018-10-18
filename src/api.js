afad521fda3c155687c4d6822595a5a5ba6144a1
Christina <christina.elaine.yuen@gmail.com>
2018-10-17 21:17:05 -0700

    api filled

---
+++ src/api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const url = 'http://jst.edchavez.com'; 
+
+export default {
+  inventory: {
+    getAll: () => axios.get(`${url}/api/inventory/getInventory`),
+  },
+
+  promotion: {
+    getAll: () => axios.get(`${url}/api/promo`),
+    getOne: ({ promoId }) => axios.get(`${url}/api/promo/${promoId}`) ,
+  }, 
+
+  shipping: {
+    getAll: () => axios.get(`${url}/api/shipping`),
+    getOne: ({ shipOptionId }) => axios.get(`${url}/api/shipping/${shipOptionId}`),
+
+  }, 
+  
+  order: {
+    post: (orderDetails) => axios.post(`${url}/api/order`, orderDetails),
+  },
+}
+
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import api from '../api.js';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  componentDidMount() {
+    console.log(api)
+    return api.inventory.getAll()
+      .then(({ data }) => {
+        this.setState({inventory: data})
+      })
+  }
+
   render() {
     return (
       <div className="App">
-hello
+        {console.log(this.state.inventory)}
       </div>
     );
   }
