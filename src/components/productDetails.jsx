b4f340aa8e475d456aac720f26ba45b80625a4e1
Christina <christina.elaine.yuen@gmail.com>
2018-10-18 19:28:22 -0700

    added card to session storage

--- src/components/productDetails.jsx
+++ src/components/productDetails.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import { Grid, Image, Loader } from 'semantic-ui-react'
+import { Button, Icon, Grid, Image, Input, Loader } from 'semantic-ui-react'
 
 import api from '../api.js';
 import './productDetails.css';
 
 export default class ProductDetails extends Component {
-  state = {};
+  state = {
+    qtyOrdered: 1
+  };
 
   componentDidMount() {
     api.inventory.getAll()
@@ -16,53 +18,87 @@ export default class ProductDetails extends Component {
       })
   }
 
-  renderLoaderOrGrid = () => {
-    if (this.state.product) {
-      return (
-        <Grid stackable divided='vertically'>
-          <Grid.Row columns={2}>
-            <Grid.Column textAlign="right">
-              <Image src='https://loremflickr.com/320/240/dog' alt='Photo of a dog'/>
-            </Grid.Column>
-            <Grid.Column>
-              <h2>
-                {this.state.product.name}
-              </h2>
-              <h3>
-                ${this.state.product.price}
-              </h3>
-              <span>
-                {this.state.product.inStock 
-                  ? 'In stock'
-                  : 'Out of stock'}
-              </span>
-            </Grid.Column>
-          </Grid.Row>
+  handleAddToCart = () => {
+    let addToCartDetails = {
+      ...this.state.product,
+      qtyOrdered: this.state.qtyOrdered,
+    };
+    delete addToCartDetails.inStock;
+    window.sessionStorage.setItem(`${this.state.product.itemId}`, JSON.stringify(addToCartDetails));
+  }
 
-          <Grid.Row columns={1}>
-            <Grid.Column>
-              <h3>
-                Description
-              </h3>
-              <span>
-                {this.state.product.description}
-              </span>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
+  handleInputChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  renderQtyInput = () => {
+    if (this.state.product.inStock) {
+      return (
+        <div>
+          In Stock
+          <Input
+            label='Qty.'
+            placeholder='1'
+            defaultValue='1'
+            name='qtyOrdered'
+            onChange={this.handleInputChange}
+            size='tiny'
+          />
+
+          <Button 
+            animated 
+            color='blue'
+            onClick={this.handleAddToCart}
+            >
+            <Button.Content visible>Add To Cart</Button.Content>
+            <Button.Content hidden>
+              {`Add ${this.state.qtyOrdered} To Cart`}
+            </Button.Content>
+          </Button>
+        </div>
       )
     } else {
       return (
-        <Loader active size='huge' />
+        <span>
+          Out of Stock
+        </span>
       )
     }
   }
 
-
   render() {
     return (
       <div>
-        {this.renderLoaderOrGrid()}
+        {this.state.product
+          ? <Grid stackable divided='vertically'>
+            <Grid.Row columns={2}>
+              <Grid.Column textAlign="right">
+                <Image src='https://loremflickr.com/320/240/dog' alt='Photo of a dog' />
+              </Grid.Column>
+              <Grid.Column>
+                <h2>
+                  {this.state.product.name}
+                </h2>
+                <h3>
+                  ${this.state.product.price}
+                </h3>
+                {this.renderQtyInput()}
+              </Grid.Column>
+            </Grid.Row>
+
+            <Grid.Row columns={1}>
+              <Grid.Column>
+                <h3>
+                  Description
+              </h3>
+                <span>
+                  {this.state.product.description}
+                </span>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+          : <Loader active size='huge' />
+        }
       </div>
     )
   }
